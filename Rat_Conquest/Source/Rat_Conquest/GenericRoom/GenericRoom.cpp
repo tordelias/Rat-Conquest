a56f4664b1285bda9402a0d889566fedfa910598
Hans <111863567+HansPluss@users.noreply.github.com>
2025-01-19 21:12:20 +0100

    Fixed room script

--- Rat_Conquest/Source/Rat_Conquest/GenericRoom/GenericRoom.cpp
+++ Rat_Conquest/Source/Rat_Conquest/GenericRoom/GenericRoom.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "GenericRoom/GenericRoom.h"
+#include "GenericRoom.h"
 
 // Sets default values
 AGenericRoom::AGenericRoom()
