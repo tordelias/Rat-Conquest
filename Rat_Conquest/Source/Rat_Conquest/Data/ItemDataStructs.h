d2a07399fc314a37d468dcef3e48efdfb4de3fff
Hans <111863567+HansPluss@users.noreply.github.com>
2025-01-24 16:37:12 +0100

    Added ItemDataTable class
    Co-Authored-By: Tord-Elias <50523683+tordelias@users.noreply.github.com>

---
+++ Rat_Conquest/Source/Rat_Conquest/Data/ItemDataStructs.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "ItemDataStructs.generated.h"
+
+UENUM()
+enum class EItemQuality : uint8
+{
+	Trash UMETA(DisplayName = "Trash"),
+	Common UMETA(DisplayName = "Common"),
+	Uncommon UMETA(DisplayName = "Uncommon"),
+	Rare UMETA(DisplayName = "Rare"),
+	SuperRare UMETA(DisplayName = "SuperRare"),
+	Epic UMETA(DisplayName = "Epic"),
+	Legendary UMETA(DisplayName = "Legendary"),
+	Mythic UMETA(DisplayName = "Mythic"),
+	DemiGod UMETA(DisplayName = "DemiGod"),
+	God UMETA(DisplayName = "God")
+};
+
+UENUM()
+enum class EItemType : uint8
+{
+	Armor UMETA(DisplayName = "Armor"),
+	Weapon UMETA(DisplayName = "Weapon"),
+	Shield UMETA(DisplayName = "Shield"),
+	Spell UMETA(DisplayName = "Spell"),
+	Consumable UMETA(DisplayName = "Consumable")
+};
+
+USTRUCT()
+struct FItemStatistics
+{
+	GENERATED_USTRUCT_BODY()
+
+
+	UPROPERTY(EditAnywhere)
+	float Defence;
+	
+	UPROPERTY(EditAnywhere)
+	float Damage;
+
+	UPROPERTY(EditAnywhere)
+	float Range;
+
+	UPROPERTY(EditAnywhere)
+	float MovementSpeed;
+
+	UPROPERTY(EditAnywhere)
+	float Maxhealth;
+
+	UPROPERTY(EditAnywhere)
+	float Luck;
+
+
+
+	UPROPERTY(EditAnywhere)
+	bool bIsMelee; 
+
+
+	UPROPERTY(EditAnywhere)
+	float SellValue;
+};
+
+USTRUCT()
+struct FItemTextData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere)
+	FText Name;
+
+	UPROPERTY(EditAnywhere)
+	FText Description;
+
+	UPROPERTY(EditAnywhere)
+	FText IteractionText;
+
+	UPROPERTY(EditAnywhere)
+	FText UsageText;
+};
+
+USTRUCT()
+struct FItemAssetData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere)
+	UTexture2D* Icon;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMesh* Mesh;
+};
+
+USTRUCT()
+struct FItemData : public FTableRowBase
+{
+	GENERATED_USTRUCT_BODY()
+
+
+	UPROPERTY(EditAnywhere, Category = "ItemData")
+	FName ID; 
+
+	UPROPERTY(EditAnywhere, Category = "ItemData")
+	EItemType ItemType;
+
+	UPROPERTY(EditAnywhere, Category = "ItemData")
+	EItemQuality ItemQuality;
+
+	UPROPERTY(EditAnywhere, Category = "ItemData")
+	FItemStatistics ItemStatistics;
+
+	UPROPERTY(EditAnywhere, Category = "ItemData")
+	FItemTextData ItemTextData;
+
+	UPROPERTY(EditAnywhere, Category = "ItemData")
+	FItemAssetData ItemAssetData;
+
+
+};
